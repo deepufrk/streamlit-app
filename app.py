0569aab50120cddc2dfc148dcbda0da67c5d1826
deepu <deepuchembra@gmail.com>
2025-04-24 14:43:29 +0530

    Add diagram

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import altair as alt
 
 st.title("My First Streamlit App")
 st.write("Welcome to my dashboard!")
@@ -8,4 +10,10 @@ st.write(f"You selected: {number}")
 
 if st.button("Click me!"):
     st.write("Button clicked!")
-#######################################
+
+data = pd.DataFrame({
+    'x': [1, 2, 3, 4, 5],
+    'y': [10, 20, 25, 20, 10]
+})
+chart = alt.Chart(data).mark_line().encode(x='x', y='y')
+st.altair_chart(chart, use_container_width=True)
